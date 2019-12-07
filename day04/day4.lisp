5503a6b9f60aed0540e7972f0d0bcf9a33aca744
Travis <tssund93@gmail.com>
2019-12-07 00:47:17 -0600

    finished day 4

--- day04/day4.lisp
+++ day04/day4.lisp
@@ -3,8 +3,11 @@
 
 (in-package :day-4)
 
-(defun check-password (number)
-  (let ((number-char-list (coerce (write-to-string number) 'list)))
+(defun number-to-char-list (number)
+  (coerce (write-to-string number) 'list))
+
+(defun check-password-part-1 (number)
+  (let ((number-char-list (number-to-char-list number)))
     (and (apply #'char-not-greaterp number-char-list)
          (has-sequential-digits-p number-char-list))))
 
@@ -13,5 +16,20 @@
         when (eql char-1 char-2)
           return t))
 
-(defun count-valid-passwords-in-range (min max)
-  (length (loop for i from min to max when (check-password i) collect i)))
+(defun count-valid-passwords-in-range-part-1 (min max)
+  (length (loop for i from min to max when (check-password-part-1 i) collect i)))
+
+(defun has-exactly-two-sequential-digits-p (number-char-list)
+  (loop for (char-1 char-2 char-3 char-4) in (maplist #'identity (cons nil number-char-list))
+        when (and (eql char-2 char-3)
+                  (not (eql char-1 char-2))
+                  (not (eql char-3 char-4)))
+          return t))
+
+(defun check-password-part-2 (number)
+  (let ((number-char-list (number-to-char-list number)))
+    (and (apply #'char-not-greaterp number-char-list)
+         (has-exactly-two-sequential-digits-p number-char-list))))
+
+(defun count-valid-passwords-in-range-part-2 (min max)
+  (length (loop for i from min to max when (check-password-part-2 i) collect i)))
