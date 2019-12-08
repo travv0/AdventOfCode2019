bce204bd2106b9c5f71cbf5ab39ca24a96a7de76
Travis Sunderland <travis@sunderland.io>
2019-12-08 00:02:04 -0600

    working on day 5 part 1

--- day05/day5.lisp
+++ day05/day5.lisp
@@ -11,21 +11,47 @@
        (split-sequence #\,)
        (map 'vector #'parse-integer)))
 
-(defun handle-opcode (f ints current-opcode-index)
+(defun handle-arithmetic-opcode (f ints current-opcode-index)
   (let ((pos-1 (aref ints (+ current-opcode-index 1)))
         (pos-2 (aref ints (+ current-opcode-index 2)))
         (result-pos (aref ints (+ current-opcode-index 3))))
     (setf (aref ints result-pos)
-          (funcall f (aref ints pos-1) (aref ints pos-2)))))
+          (funcall f (aref ints pos-1) (aref ints pos-2))))
+  4)
+
+(defun handle-input-opcode (ints current-opcode-index)
+  (format t "> ")
+  (force-output)
+  (let ((pos (aref ints (1+ current-opcode-index)))
+        (input (parse-integer (read-line) :junk-allowed t)))
+    (setf (aref ints pos) input))
+  2)
+
+(defun handle-output-opcode (ints current-opcode-index)
+  (let ((pos (aref ints (1+ current-opcode-index))))
+    (print (aref ints pos)))
+  2)
 
 (defun process-intcode (ints)
-  (loop for i = 0 then (+ i 4)
-        until (= (aref ints i) 99)
-        do (let ((opcode (aref ints i)))
-             (cond ((= opcode 1) (handle-opcode #'+ ints i))
-                   ((= opcode 2) (handle-opcode #'* ints i))
-                   (t (error (format nil "Invalid opcode: ~a" opcode))))))
+  (let ((inc 0))
+    (loop for i = 0 then (+ i inc)
+          until (= (aref ints i) 99)
+          do (let ((opcode (process-opcode (aref ints i))))
+               (setf inc
+                     (case opcode
+                       (1 (handle-arithmetic-opcode #'+ ints i))
+                       (2 (handle-arithmetic-opcode #'* ints i))
+                       (3 (handle-input-opcode ints i))
+                       (4 (handle-output-opcode ints i))
+                       (otherwise (error (format nil "Invalid opcode: ~a" opcode))))))))
   (aref ints 0))
+
+(defun process-opcode (opcode)
+  (values (mod opcode 100)
+          (loop for i = (floor (/ opcode 100)) then (floor (/ i 10))
+                for j = 0 then (incf j)
+                while (< j 3)
+                collecting (mod i 10))))
 
 (defun set-state (ints noun verb)
   (setf (aref ints 1) noun
@@ -44,8 +70,7 @@
   (let* ((ints (with-open-file (input "input.txt")
                  (-> input
                      (read-line nil)
-                     parse-input
-                     (set-state 12 2)))))
+                     parse-input))))
     (cond ((= part 1) (process-intcode ints))
           ((= part 2) (find-noun-and-verb ints 19690720))
           (t (error "`part' must be either 1 or 2")))))
