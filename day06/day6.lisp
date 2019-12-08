46f88ff82a8f78741c1d5fda4d1dfc0facd2b78c
Travis <tssund93@gmail.com>
2019-12-08 14:52:27 -0600

    day 6 part 2

--- day06/day6.lisp
+++ day06/day6.lisp
@@ -7,14 +7,13 @@
 
 (in-package :day-6)
 
-(defun count-orbits-for-planet (planet-vertex)
-  (let ((children (child-vertexes planet-vertex)))
-    (if children
-        (1+ (reduce #'+ (mapcar #'count-orbits-for-planet children)))
-        0)))
+(defun list-orbiting-planet-vertexes (planet-vertex)
+  (let ((child (first (child-vertexes planet-vertex))))
+    (when child
+        (cons child (list-orbiting-planet-vertexes child)))))
 
 (defun count-total-orbits (orbits)
-  (reduce #'+ (mapcar #'count-orbits-for-planet (vertexes orbits))))
+  (reduce #'+ (mapcar (compose #'length #'list-orbiting-planet-vertexes) (vertexes orbits))))
 
 (defun input-to-orbits (lines)
   (let ((orbits (make-container 'graph-container :default-edge-type :directed)))
@@ -22,9 +21,22 @@
           do (add-edge-between-vertexes orbits (intern outer) (intern inner)))
     orbits))
 
-(defun main ()
-  (let ((lines (with-open-file (input "input.txt")
+(defun find-number-of-orbital-transfers (orbits start end)
+  (let ((vertexes-from-start (list-orbiting-planet-vertexes (find-vertex orbits start)))
+        (vertexes-from-end (list-orbiting-planet-vertexes (find-vertex orbits end))))
+    (loop for vertex in vertexes-from-start
+          for i = 0 then (1+ i)
+          for pos = (position vertex vertexes-from-end)
+          when pos
+            return (+ pos i))))
+
+(defun main (&key (part 2))
+  (let* ((lines (with-open-file (input "input.txt")
                  (loop for line = (read-line input nil)
                        while line
-                       collect line))))
-    (count-total-orbits (input-to-orbits lines))))
+                       collect line)))
+         (orbits (input-to-orbits lines)))
+    (case part
+      (1 (count-total-orbits orbits))
+      (2 (find-number-of-orbital-transfers orbits 'you 'san))
+      (otherwise (error "`part' must be either 1 or 2")))))
