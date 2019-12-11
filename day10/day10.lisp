3a30a8ce7d2711c456890fdcdfd52f9943007c26
Travis <tssund93@gmail.com>
2019-12-10 18:15:42 -0600

    day 10 part 2

--- day10/day10.lisp
+++ day10/day10.lisp
@@ -60,9 +60,84 @@
                     max-visible-asteroids visible-asteroids))))))
     (values optimal-asteroid max-visible-asteroids)))
 
-(defun main ()
-  (let* ((*asteroid-map* (parse-input (read-file-into-string "input.txt"))))
-    (get-optimal-asteroid)))
+(defun calculate-angle (origin-asteroid dest-asteroid)
+  (let* ((origin-x (first origin-asteroid))
+         (origin-y (second origin-asteroid))
+         (dest-x (first dest-asteroid))
+         (dest-y (second dest-asteroid))
+         (dx (- origin-x dest-x))
+         (dy (- origin-y dest-y)))
+    (loop for angle = (- (* (atan dy dx)
+                            (/ 180 pi))
+                         90)
+            then (+ angle 360)
+          when (> angle 0)
+            return angle)))
+
+(defun calculate-distance (origin-asteroid dest-asteroid)
+  (let* ((origin-x (first origin-asteroid))
+         (origin-y (second origin-asteroid))
+         (dest-x (first dest-asteroid))
+         (dest-y (second dest-asteroid))
+         (dx (- origin-x dest-x))
+         (dy (- origin-y dest-y)))
+    (sqrt (+ (expt dx 2) (expt dy 2)))))
+
+(defun build-asteroid-coord-list (exclude-x exclude-y)
+  (let ((asteroid-coord-list '()))
+    (loop for y from 0 below (array-dimension *asteroid-map* 0) do
+      (loop for x from 0 below (array-dimension *asteroid-map* 1) do
+        (when (and (not (and (= exclude-x x) (= exclude-y y)))
+                   (asteroid-p x y))
+          (setf asteroid-coord-list (cons (list x y) asteroid-coord-list)))))
+    asteroid-coord-list))
+
+(defun build-angles-and-distances-list (origin-coords coords-list)
+  (let ((coords-hash (make-hash-table)))
+    (mapc (lambda (coords)
+            (let ((angle (calculate-angle origin-coords coords)))
+              (setf (gethash angle coords-hash)
+                    (cons (list :coords coords
+                                :angle (calculate-angle origin-coords coords)
+                                :distance (calculate-distance origin-coords coords))
+                          (gethash angle coords-hash '())))))
+          coords-list)
+    (sort (mapcar (lambda (l)
+                    (sort l (lambda (e1 e2) (< (getf e1 :distance)
+                                               (getf e2 :distance)))))
+                  (hash-table-values coords-hash))
+          (lambda (l1 l2) (< (getf (car l1) :angle)
+                             (getf (car l2) :angle))))))
+
+(defun flatten-ordered-destruction-list (angles-and-distances-list)
+  (let ((flattened-list '()))
+    (loop for i = 0 then (mod (1+ i) (length angles-and-distances-list))
+          for e = (nth i angles-and-distances-list)
+          until (every #'not angles-and-distances-list)
+          when e
+            do (setf flattened-list (cons (car e) flattened-list)
+                     (nth i angles-and-distances-list) (cdr e))
+          finally (return (reverse flattened-list)))))
+
+(defun get-200th-asteroid-to-be-destroyed (origin-coords)
+  (let ((x (first origin-coords))
+        (y (second origin-coords)))
+    (destructuring-bind (result-x result-y)
+        (getf
+         (nth 199
+              (flatten-ordered-destruction-list
+               (build-angles-and-distances-list origin-coords
+                                                (build-asteroid-coord-list x y))))
+         :coords)
+      (+ (* result-x 100) result-y))))
+
+(defun main (&key (part 2))
+  (let ((*asteroid-map* (parse-input (read-file-into-string "input.txt"))))
+    (multiple-value-bind (optimal-asteroid visible-asteroids) (get-optimal-asteroid)
+      (case part
+        (1 visible-asteroids)
+        (2 (get-200th-asteroid-to-be-destroyed optimal-asteroid))
+        (otherwise (error ("`part' must be either 1 or 2")))))))
 
 (defun tests ()
   (let ((*asteroid-map* (parse-input ".#..#
