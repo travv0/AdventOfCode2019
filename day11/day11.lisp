8f05cbd5bd8082a45a4d78b1a724cb41973cf443
Travis Sunderland <travis@sunderland.io>
2019-12-11 20:46:10 -0600

    day 11 part 2

--- day11/day11.lisp
+++ day11/day11.lisp
@@ -24,8 +24,6 @@
              :initform (cons 0 0)
              :accessor robot-position)))
 
-(defparameter *panels* (make-hash-table :test 'equal))
-
 (defmethod rotate-clockwise ((robot robot))
   (setf (direction robot) (nth (mod (1+ (or (position (direction robot)
                                                       +directions+)
@@ -52,21 +50,40 @@
       (:left (decf (car pos))))
     pos))
 
-(defun main ()
+(defun draw-panels (panels)
+  (let* ((keys (hash-table-keys panels))
+         (xs (mapcar #'car keys))
+         (ys (mapcar #'cdr keys))
+         (min-x (apply #'min xs))
+         (max-x (apply #'max xs))
+         (min-y (apply #'min ys))
+         (max-y (apply #'max ys)))
+    (loop for y from min-y to max-y do
+      (loop for x from min-x to max-x do
+        (format t "~a" (case (gethash (cons x y) panels 0)
+                         (0 " ")
+                         (1 "."))))
+      (format t "~%"))))
+
+(defun main (&key (part 2))
   (let* ((intcode (parse-input (read-file-into-string "input.txt")))
          (computer (make-computer intcode))
          (robot (make-instance 'robot))
-         (*panels* (make-hash-table :test 'equal)))
+         (panels (make-hash-table :test 'equal)))
+    (when (= part 2)
+      (setf (gethash (cons 0 0) panels) 1))
     (loop
       (multiple-value-bind (output complete)
-          (run-computer computer (list (gethash (robot-position robot) *panels* 0)))
+          (run-computer computer (list (gethash (robot-position robot) panels 0)))
         (let ((color-code (first output))
               (rotation-code (second output))
               (position (robot-position robot)))
-          (setf (gethash (copy-list position) *panels*) color-code)
+          (setf (gethash (copy-list position) panels) color-code)
           (case rotation-code
             (0 (rotate-counterclockwise robot))
             (1 (rotate-clockwise robot)))
           (move robot)
           (when complete
-            (return (hash-table-count *panels*))))))))
+            (return (case part
+                      (1 (hash-table-count panels))
+                      (2 (draw-panels panels))))))))))
