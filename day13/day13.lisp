4a46c9b7a384116a15de4aa4b7ece8272e6b05cb
Travis <tssund93@gmail.com>
2019-12-14 18:43:59 -0600

    day 13 part 2

--- day13/day13.lisp
+++ day13/day13.lisp
@@ -3,17 +3,15 @@
 
 (in-package :day13)
 
-(defun display-game (tiles)
-  (let* ((keys (hash-table-keys tiles))
+(defun display-game (game-state)
+  (let* ((keys (hash-table-keys game-state))
          (xs (mapcar #'car keys))
          (ys (mapcar #'cdr keys))
-         (min-x (apply #'min xs))
          (max-x (apply #'max xs))
-         (min-y (apply #'min ys))
          (max-y (apply #'max ys)))
-    (loop for y from min-y to max-y do
-      (loop for x from min-x to max-x do
-        (let ((tile-id (gethash (cons x y) tiles 0)))
+    (loop for y from 0 to max-y do
+      (loop for x from 0 to max-x do
+        (let ((tile-id (gethash (cons x y) game-state 0)))
           (format t "~a" (case tile-id
                            (0 " ")
                            (1 "|")
@@ -23,13 +21,48 @@
                            (otherwise (error (format nil "Invalid tile id: ~a" tile-id)))))))
       (format t "~%"))))
 
-(defun update-game (input)
-  (let ((tiles (make-hash-table :test 'equal)))
-    (loop for (x y tile-id) on input by #'cdddr do
-      (setf (gethash (cons x y) tiles) tile-id))
-    tiles))
+(defun update-game (game-state input)
+  (loop for (x y tile-id) on input by #'cdddr do
+    (setf (gethash (cons x y) game-state) tile-id))
+  game-state)
 
-(defun main ()
-  (let* ((computer (make-computer (parse-input (read-file-into-string "input.txt"))))
-         (game-state (update-game (run-computer computer))))
-    (count-if (lambda (id) (= id 2)) (hash-table-values game-state))))
+(defun count-blocks (game-state)
+  (count-if (lambda (id) (= id 2)) (hash-table-values game-state)))
+
+(defun game-complete-p (game-state)
+  (= 0 (count-blocks game-state)))
+
+(defun play-game (computer)
+  (setf (access-address computer 0) 2)
+  (loop with game-state = (update-game (make-hash-table :test 'equal)
+                                       (run-computer computer))
+        when (game-complete-p game-state)
+          return (gethash (cons -1 0) game-state)
+        do (let* ((ball-x (ball-x game-state))
+                  (paddle-x (paddle-x game-state))
+                  (new-game-state (update-game game-state
+                                               (run-computer computer
+                                                             (list (cond ((< ball-x paddle-x) -1)
+                                                                         ((> ball-x paddle-x) 1)
+                                                                         (t 0)))))))
+             (setf game-state new-game-state))))
+
+(defun tile-x (game-state tile-id)
+  (loop for ((x . y) . id) in (hash-table-alist game-state)
+        when (= tile-id id)
+          return x))
+
+(defun ball-x (game-state)
+  (tile-x game-state 4))
+
+(defun paddle-x (game-state)
+  (tile-x game-state 3))
+
+(defun main (&key (part 2))
+  (let ((computer (make-computer (parse-input (read-file-into-string "input.txt")))))
+    (case part
+      (1 (let ((game-state (update-game (make-hash-table :test 'equal)
+                                        (run-computer computer))))
+           (count-blocks game-state)))
+      (2 (play-game computer))
+      (otherwise (error "`part' must be either 1 or 2")))))
