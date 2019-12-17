e6cbe660a81c908a1a53f1cf76d8a81dcf142afe
Travis Sunderland <tsunderland@rx-ss.com>
2019-12-17 13:30:38 -0600

    day 17 part 1

--- day17/day17.lisp
+++ day17/day17.lisp
@@ -5,24 +5,34 @@
 
 (in-package :day17)
 
-(defun convert-computer-output (input)
-  (let ((lines (str:lines (map 'string #'code-char input))))
+(defun convert-computer-output (output)
+  (let ((lines (str:lines (map 'string #'code-char output) :omit-nulls t)))
     (make-array (list (length lines) (length (car lines)))
                 :initial-contents lines)))
 
 (defun calculate-alignment-parameters (scaffolding)
-  (loop for y from 0 below (array-dimension scaffolding 0) do
-    (loop for x from 0 below (second (array-dimensions scaffolding))
-          when (is-intersection scaffolding x y)
-            sum (* x y))))
+  (loop for y from 0 below (array-dimension scaffolding 0)
+        sum (loop for x from 0 below (second (array-dimensions scaffolding))
+                  when (is-intersection scaffolding x y)
+                    sum (* x y))))
 
 (defun is-intersection (scaffolding x y)
   (when (and (> x 0)
              (> y 0)
-             (< x (- (second (array-dimensions scaffolding)) 3))
-             (< y (- (array-dimension scaffolding 0) 2)))
+             (< x (1- (second (array-dimensions scaffolding))))
+             (< y (1- (array-dimension scaffolding 0))))
     (every (lambda (c) (char= c #\#))
-           (list (aref scaffolding (1- x) y)
-                 (aref scaffolding (1+ x) y)
-                 (aref scaffolding x (1- y))
-                 (aref scaffolding x (1+ y))))))
+           (list (aref scaffolding y x)
+                 (aref scaffolding (1- y) x)
+                 (aref scaffolding (1+ y) x)
+                 (aref scaffolding y (1- x))
+                 (aref scaffolding y (1+ x))))))
+
+(defun main (&key (part 1))
+  (let* ((computer (make-computer
+                    (parse-input (read-file-into-string "input.txt"))))
+         (scaffolding (convert-computer-output (run-computer computer))))
+    (case part
+      (1 (calculate-alignment-parameters scaffolding))
+      (2 (error "unimplemented"))
+      (otherwise (error "`part' must be either 1 or 2")))))
