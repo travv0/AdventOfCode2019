fbf4b5e3caf6004e41226c8c33ad402f82247693
Travis <tssund93@gmail.com>
2019-12-21 22:37:40 -0600

    day 21 part 2

--- day21/day21.lisp
+++ day21/day21.lisp
@@ -3,7 +3,7 @@
 
 (in-package #:day21)
 
-(defparameter *springscript* (format nil "~
+(defparameter *springscript-walk* (format nil "~
 NOT A J
 NOT B T
 OR T J
@@ -13,9 +13,27 @@ AND D J
 WALK
 "))
 
-(defun main ()
+(defparameter *springscript-run* (format nil "~
+NOT A J
+NOT B T
+OR T J
+NOT C T
+OR T J
+AND D J
+AND H T
+OR H T
+OR E T
+AND T J
+RUN
+"))
+
+(defun main (&key (part 2))
   (let* ((computer (make-computer (parse-input (read-file-into-string "input.txt"))))
-         (result (run-computer computer (map 'list #'char-code *springscript*)))
+         (result (run-computer computer (map 'list #'char-code
+                                             (case part
+                                               (1 *springscript-walk*)
+                                               (2 *springscript-run*)
+                                               (otherwise "`part' must be either 1 or 2")))))
          (last-digit (car (last result))))
     (if (> last-digit 255)
         (format t "~a~%" last-digit)
