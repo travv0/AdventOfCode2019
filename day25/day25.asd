c3eaaf0e3a11a9620b074a54177849f234ea15ca
Travis <tssund93@gmail.com>
2019-12-25 00:12:12 -0600

    day 25

---
+++ day25/day25.asd
@@ -0,0 +1,5 @@
+(asdf:defsystem #:day25
+  :serial t
+  :depends-on (:alexandria :intcode-interpreter)
+  :pathname "./"
+  :components ((:file "day25")))
---
+++ day25/day25.lisp
@@ -0,0 +1,15 @@
+(defpackage #:day25
+  (:use #:cl #:alexandria #:intcode-interpreter))
+
+(in-package #:day25)
+
+(defun main ()
+  (let ((computer (make-computer (parse-input (read-file-into-string "input.txt")))))
+    (format t "~a~%" (map 'string #'code-char (run-computer computer)))
+    (loop
+      (format t "> ")
+      (multiple-value-bind (output done)
+          (run-computer computer (append (map 'list #'char-code (read-line)) '(10)))
+        (format t "~a~%" (map 'string #'code-char output))
+        (when done
+          (return))))))
