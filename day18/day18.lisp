16920ce93b15599737b238ea73d47898948c9b20
Travis <tssund93@gmail.com>
2019-12-26 00:48:59 -0600

    day 18 part 1

--- day18/day18.lisp
+++ day18/day18.lisp
@@ -3,8 +3,6 @@
 
 (in-package #:day18)
 
-(declaim (optimize speed (debug 1) safety))
-
 (defun parse-input (input)
   (loop with map = (make-hash-table :test 'equal)
         for line in (str:lines input)
@@ -19,7 +17,7 @@
   path-length
   total-length-estimate)
 
-(defun find-shortest-path (start goal-func successors heuristic-dist)
+(defun find-shortest-path (start goal successors heuristic-dist)
   (do (head-node		; node at head of open list
        path-to-extend         ; path to state currently visited
        current-state		; state currently visited
@@ -28,13 +26,13 @@
         (list (make-node :path (list start)
                          :path-length 0
                          :total-length-estimate
-                         (funcall heuristic-dist start))))
-       (state-distances (make-hash-table :test #'equalp)))
+                         (funcall heuristic-dist start goal))))
+       (state-distances (make-hash-table :test #'equal)))
       ((null open) nil)         ; if open list is empty, search fails
     (setq head-node (pop open))       ; get node at head of open list
     (setq path-to-extend (node-path head-node)) ; get path itself
     (setq current-state (car path-to-extend)) ; get state this path ends at
-    (when (funcall goal-func current-state)
+    (when (equal current-state goal)
       (return head-node))	; success: return path and length found
     (setq dist-so-far (node-path-length head-node))
     (when (less-than dist-so-far (gethash current-state state-distances))
@@ -55,7 +53,7 @@
                      :path-length next-dist-so-far
                      :total-length-estimate
                      (+ next-dist-so-far
-                        (funcall heuristic-dist next-state))))
+                        (funcall heuristic-dist next-state goal))))
                    open
                    #'<
                    :key #'node-total-length-estimate))))))))
@@ -68,56 +66,98 @@
   (or (null y) (< x y)))
 
 (defun get-neighbors (map pos)
-  (destructuring-bind (pos . keys) pos
-    (declare ((or cons null) keys))
-    (let ((neighbors (remove-if (lambda (coords)
-                                  (let ((tile (gethash coords map #\#)))
-                                    (or (char= tile #\#)
-                                        (and (char<= #\A tile #\Z)
-                                             (not (position tile keys :test 'equalp))))))
-                                (list
-                                 (cons (car pos) (1- (cdr pos)))
-                                 (cons (car pos) (1+ (cdr pos)))
-                                 (cons (1- (car pos)) (cdr pos))
-                                 (cons (1+ (car pos)) (cdr pos))))))
-      (mapcar (lambda (coords)
-                (let ((tile (gethash coords map #\.)))
-                  (cons (cons coords (if (and (char<= #\a tile #\z)
-                                              (not (position tile keys)))
-                                         (cons tile keys)
-                                         keys))
-                        1)))
-              neighbors))))
-
-(defun get-heuristic (key-coords number-of-keys pos)
-  (+ (- number-of-keys (length (cdr pos)))
-     (loop for coords in key-coords
-           minimizing (get-distance (car pos) coords))))
+  (let ((neighbors (remove-if (lambda (coords)
+                                (let ((tile (gethash coords map #\#)))
+                                  (char= tile #\#)))
+                              (list
+                               (cons (car pos) (1- (cdr pos)))
+                               (cons (car pos) (1+ (cdr pos)))
+                               (cons (1- (car pos)) (cdr pos))
+                               (cons (1+ (car pos)) (cdr pos))))))
+    (mapcar (rcurry #'cons 1) neighbors)))
 
 (defun get-distance (start end)
   (+ (abs (- (car start) (car end)))
      (abs (- (cdr start) (cdr end)))))
 
-(defun find-path (map pos)
-  (let* ((key-coords (loop for coords being the hash-keys of map
-                             using (hash-value tile)
-                           when (lower-case-p tile)
-                             collecting coords))
-         (total-keys (length key-coords))
-         (node (find-shortest-path (cons pos '())
-                                   (compose (curry #'= total-keys) #'length #'cdr)
-                                   (curry #'get-neighbors map)
-                                   (curry #'get-heuristic key-coords total-keys))))
-    (when node (reverse (node-path node)))))
-
-(defun find-start (map)
+(defun find-pos (map char)
   (loop for coords being the hash-keys of map using (hash-value tile)
-        when (char= tile #\@)
+        when (char= tile char)
           return coords))
+
+(defun get-key-positions (map)
+  (loop for tile being the hash-values of map using (hash-key pos)
+        when (lower-case-p tile)
+          collect pos))
+
+(defun get-keys (map)
+  (loop for tile being the hash-values of map
+        when (lower-case-p tile)
+          collect tile))
+
+(defun get-key-routes (map)
+  (let* ((keys (coerce (cons (find-pos map #\@) (get-key-positions map)) 'vector))
+         (key-count (length keys)))
+    (loop for i below key-count
+          append (loop for j from i below key-count
+                       collecting (list (aref keys i) (aref keys j))))))
+
+(defstruct (path :conc-name) path-end path-length key doors)
+
+(defun doors-along (map path)
+  (loop for coords in path
+        for tile = (gethash coords map)
+        when (upper-case-p tile)
+          collect tile))
+
+(defun get-paths-between-keys (map)
+  (let ((paths (make-hash-table :test 'equal)))
+    (loop for (start end) in (get-key-routes map) do
+      (let* ((node (find-shortest-path start
+                                       end
+                                       (curry #'get-neighbors map)
+                                       #'get-distance)))
+        (unless (equal start end)
+          (let ((path (make-path :path-end end
+                                 :path-length (node-path-length node)
+                                 :key (gethash end map)
+                                 :doors (doors-along map (node-path node))))
+                (start-char (gethash start map)))
+            (push path (gethash start paths))
+            (unless (char= start-char #\@)
+              (push (make-path :path-end start
+                               :path-length (path-length path)
+                               :key (gethash start map)
+                               :doors (doors path))
+                    (gethash end paths)))))))
+    paths))
+
+(defun distance-to-collect-keys (map start paths &optional
+                                                   (remaining-keys (get-keys map))
+                                                   (cache (make-hash-table :test 'equal)))
+  (cond ((null remaining-keys) 0)
+        (t (or (gethash (cons start remaining-keys) cache)
+               (setf (gethash (cons start remaining-keys) cache)
+                     (loop for path in (reachable-keys (gethash start paths) remaining-keys)
+                           minimizing (+ (path-length path)
+                                         (distance-to-collect-keys map
+                                                                   (path-end path)
+                                                                   paths
+                                                                   (remove (key path) remaining-keys)
+                                                                   cache))))))))
+
+(defun reachable-keys (paths remaining-keys)
+  (remove-if-not (lambda (path)
+                   (position (key path) remaining-keys))
+                 (remove-if (lambda (path)
+                              (some #'(lambda (door)
+                                        (position (char-downcase door) remaining-keys))
+                                    (doors path)))
+                            paths)))
 
 (defun main (&key (part 1))
   (let ((map (parse-input (read-file-into-string "input.txt"))))
     (case part
-      (1 (length (cdr (find-path map (find-start map)))))
+      (1 (distance-to-collect-keys map (find-pos map #\@) (get-paths-between-keys map)))
       (2 (error "unimplemented"))
       (otherwise (error "`part' must be either 1 or 2")))))
